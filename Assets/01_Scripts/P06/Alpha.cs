4cca7a8d340282a2627c0dd015be814aea8a7b8f
prog7602 <>
2023-11-07 18:16:09 +0900

    P06 挙動完成

---
+++ Assets/01_Scripts/P06/Alpha.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Alpha : MonoBehaviour
+{
+    public float changeSpeed = 0.01f;
+    public GameObject hito;
+    public SpriteRenderer image;
+    private Color imageColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = hito.GetComponent<SpriteRenderer>();
+        imageColor = image.color;
+        imageColor.a = 0;
+        image.color = imageColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (imageColor.a < 1f)
+        {
+            imageColor = image.color;
+            imageColor.a += changeSpeed;
+            image.color = imageColor;
+        }
+    }
+}
