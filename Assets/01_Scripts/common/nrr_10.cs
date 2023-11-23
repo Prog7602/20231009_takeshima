541796cb60c9e62fbb97f1a554699f79d21d6c5a
Prog7602 <proken7602@gmail.com>
2023-11-23 18:12:23 +0900

    ネクストバルーンにスクリプト追加。 15秒経過でネクストバルーンが表示されるように。 また、nrr_10を他のページのネクストバルーンでも使用できるように改良した。

---
+++ Assets/01_Scripts/common/nrr_10.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class nrr_10 : MonoBehaviour
+{
+    Image sp;
+
+    public float timer;
+
+    void Start()
+    {
+        sp = this.GetComponent<Image>();
+        Invoke("appear_tugie", timer);
+    }
+
+    void appear_tugie()
+    {
+        sp.color = new Color(1, 1, 1, 255);
+        Debug.Log(timer + "秒経過");
+    }
+}
