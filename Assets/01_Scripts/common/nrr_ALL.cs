0e276c3e2a897ddb9d11d9724be34a48f8d4f6e2
Prog7602 <proken7602@gmail.com>
2023-11-28 18:22:39 +0900

    nrr_ALLという、全てのページのネクストバルーンにアタッチ出来るスクリプトの改良版を制作した。 また、P11で異なるPCでも変わらず画像を表示できるように、新たにキャンバスを作り、その上にUI.イメージで画像を表示させた。 また、プレハブUIのレイヤー(キャンバスをカメラに合わせるモード)をuiにすることで前面に表示させるようにした。 画像を貼る方のキャンバスのレイヤーはデフォルトにした。 後々、自身のPCでキレイに画像が表示されるかテスト予定。

---
+++ Assets/01_Scripts/common/nrr_ALL.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class nrr_ALL : MonoBehaviour
+{
+    Image sp;
+
+    public float timer; //インスペクターから朗読音声ファイルの出力時間を入力
+
+    void Start()
+    {
+        sp = this.GetComponent<Image>();
+        timer++; //１秒足すことで朗読が終わった後、丁度いいタイミングでネクストバルーンが出るようにしている。
+        Invoke("appear_tugie", timer); //一定秒数経過後、appear_tugieを読み込む。
+    }
+
+    void appear_tugie()
+    {
+        sp.color = new Color(1, 1, 1, 255); //ネクストバルーンを透明状態から非透明状態にしている。
+        Debug.Log(timer + "秒経過"); //コンソールに経過秒数を表示
+    }
+}
